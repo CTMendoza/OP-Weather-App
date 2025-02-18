1121f4c957c838f7c896a1ff05386f676cd3b591
Christopher Mendoza <tiptop1112@gmail.com>
2025-02-18 11:12:53 -0800

    Initial setup for project set up webpack for weather app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import "./styles.css";
-import { toggleDropDown } from "./toggleDropDown";
-const dropDownButton = document.querySelector('button');
-const linksContainer = document.querySelector('.link-container');
 
-toggleDropDown();
-
-export {dropDownButton, linksContainer}
