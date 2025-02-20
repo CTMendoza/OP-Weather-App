4864af5d2f64722eb1f8a6a2397be24197466a70
Christopher Mendoza <tiptop1112@gmail.com>
2025-02-20 13:18:28 -0800

    Created new function Created new function that fetches weather data from visual crossing API and then parses it into an object that is logged to the console to view.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./styles.css";
 
+function getForecast () {
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/90701?key=V2W5E3UV9S9V3X9VG8HSUMW6K`, {mode: 'cors'})
+    .then(function(response) {
+        console.log(response.json());
+      });
+}
+
+getForecast();
