eb3b18704276deff02f90dabe017d9c28c703fca
Christopher Mendoza <tiptop1112@gmail.com>
2025-02-21 14:22:19 -0800

    Stored parsed json data into a global object variable to be used later

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import "./styles.css";
 
+let location = {}
+let currentWeather
+let weeklyForecast
+
 function getForecast () {
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/90701?key=V2W5E3UV9S9V3X9VG8HSUMW6K`, {mode: 'cors'})
+    return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Cerritos,CA?key=V2W5E3UV9S9V3X9VG8HSUMW6K`, {mode: 'cors'})
     .then(function(response) {
-        console.log(response.json());
-      });
+        return response.json();
+      })
+    .then(data => {
+      currentWeather = data.currentConditions
+      weeklyForecast = data.days
+      location.currentWeather = currentWeather;
+      location.weeklyForecast = weeklyForecast;
+      console.log("Inside getForecast:", location);
+    
+    })
+    .catch(error => console.error("Error fetching data:", error));
 }
 
-getForecast();
+
+getForecast().then(() => {
+  let temp = location.currentWeather.temp;
+  console.log(`temp is ${temp}`);
+});
+
