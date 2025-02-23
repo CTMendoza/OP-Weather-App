67d543f11c9b908449289286b6bbda65303370cd
Christopher Mendoza <tiptop1112@gmail.com>
2025-02-23 11:53:50 -0800

    Created event listener for form submit button The event listener function of the submit button creates a new FormData so that I can pull the input value and use it as a parameter of the fetch method when Im getting weather data for that location off of the api. Once that happens, data received from api is stored within an empty object called location that will be used when I display the info on the webpage

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import "./styles.css";
 let location = {}
 let currentWeather
 let weeklyForecast
+const submit = document.querySelector('button');
+const form = document.querySelector('form');
 
 
-function getForecast () {
-    return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Cerritos,CA?key=V2W5E3UV9S9V3X9VG8HSUMW6K`, {mode: 'cors'})
+function getForecast (locationInput) {
+    return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInput}?key=V2W5E3UV9S9V3X9VG8HSUMW6K`, {mode: 'cors'})
     .then(function(response) {
         return response.json();
       })
@@ -15,13 +17,21 @@ function getForecast () {
       weeklyForecast = data.days
       location.currentWeather = currentWeather;
       location.weeklyForecast = weeklyForecast;
-      console.log("Inside getForecast:", location);
     })
     .catch(error => console.error("Error fetching data:", error));
 }
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const locationInput = formData.get("location") //gets value of input when form is submitted
+  getForecast(locationInput).then(() => {  // fetches location weather based on input value then logs data to console.
+    console.log(`location data is: `, location);
+  });
+  form.reset(); // resets form input values
+})
 
-getForecast().then(() => {
-  console.log(`location data is: `, location);
-});
+// getForecast().then(() => {
+//   console.log(`location data is: `, location);
+// });
 
