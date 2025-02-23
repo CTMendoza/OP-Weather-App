371c3c248e00beacf58496c4ecb139496241278c
Christopher Mendoza <tiptop1112@gmail.com>
2025-02-23 11:27:44 -0800

    Added button to form for when user submits location for weather data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ let location = {}
 let currentWeather
 let weeklyForecast
 
+
 function getForecast () {
     return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Cerritos,CA?key=V2W5E3UV9S9V3X9VG8HSUMW6K`, {mode: 'cors'})
     .then(function(response) {
@@ -15,14 +16,12 @@ function getForecast () {
       location.currentWeather = currentWeather;
       location.weeklyForecast = weeklyForecast;
       console.log("Inside getForecast:", location);
-    
     })
     .catch(error => console.error("Error fetching data:", error));
 }
 
 
 getForecast().then(() => {
-  let temp = location.currentWeather.temp;
-  console.log(`temp is ${temp}`);
+  console.log(`location data is: `, location);
 });
 
